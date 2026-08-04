10fcee24e87529b5f2378a8989e95bde45a673d4
TadalaVijayaDurgaPrasad <155704320+TadalaVijayaDurgaPrasad@users.noreply.github.com>
2026-08-04 17:44:34 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,8 +0,0 @@
-variable "servers" {
-  description = "Map of EC2 instance definitions to create"
-  type = map(object({
-    ami_id        = string
-    instance_type = string
-    name          = string
-  }))
-}
