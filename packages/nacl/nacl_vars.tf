ad8fd830431c250d409255678c73831174adb22a
vijay <vijay@devops.com>
2026-08-04 20:27:11 +0530

    nacl included

---
+++ packages/nacl/nacl_vars.tf
@@ -0,0 +1,28 @@
+variable "network_acl_details" {
+    description = "Details of the Network ACL"
+    type = map(object({
+        vpc_id = string
+        network_acl_name = string
+        subnets = list(string)
+        egress_rules = map(object({
+            rule_no = number
+            protocol = string
+            action = string
+            cidr_block = string
+            from_port = number
+            to_port = number
+            # egress = bool
+        }))
+        ingress_rules = map(object({
+            rule_no = number
+            protocol = string
+            action = string
+            cidr_block = string
+            from_port = number
+            to_port=number
+            # ingress = bool
+        }))
+
+    }))
+  
+}
