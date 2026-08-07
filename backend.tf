93a286ec31a3ab949171df5b443eb159825210bd
vijay <vijay@devops.com>
2026-08-07 21:27:15 +0530

    main.tf updated

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-bucket-753805950551-eu-central-1-an"
-#     key = "networking/terraform.tfstate"
-#     region = "eu-central-1"
-#     encrypt = true
-#     use_lockfile = true
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket-753805950551-eu-central-1-an"
+    key = "networking/terraform.tfstate"
+    region = "eu-central-1"
+    encrypt = true
+    use_lockfile = true
     
-#   }
-# }
+  }
+}
